7e9ad3e276cd2c701457c02a1cda2f73a68a978b
baarath <baarath.s.2016.cse@rajalakshmi.edu.in>
2019-04-05 00:54:46 +0530

    phoneno authentication added

---
+++ app/src/main/java/com/example/projectfuture/Home.java
@@ -0,0 +1,13 @@
+package com.example.projectfuture;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
